8e1925b665257a494ee41c52964179022f3f3f22
Abdul Wahab Raza <115993963+AbdulWahabRaza0@users.noreply.github.com>
2023-12-27 22:10:00 +0500

    Update client.ts

--- frontend/services/client.ts
+++ frontend/services/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 const client = axios.create({
-  baseURL: "http://localhost:5000/api/",
-  // baseURL: "https://gpt-backend-gamma.vercel.app/api/",
+  // baseURL: "http://localhost:5000/api/",
+  baseURL: "https://gpt-backend-gamma.vercel.app/api/",
 });
 
 export { client };
